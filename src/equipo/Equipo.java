2b4f2269b29a08e725bba887a778385fd1af5ebd
itsazahara <159541934+itsazahara@users.noreply.github.com>
2024-03-17 10:40:52 +0100

    La clase "equipo" ha sido modificada y de momento terminada, se irá revisando poco a poco

--- src/equipo/Equipo.java
+++ src/equipo/Equipo.java
@@ -2,4 +2,32 @@ package equipo;
 
 public class Equipo {
 
+  private Jugador[] alineacion;
+
+  public Equipo() {
+    this.alineacion = new Jugador[11];
+  }
+
+  public void añadirJugador(Jugador jugador, int posicion){
+    alineacion[posicion] = jugador;
+  }
+
+  public void mostrarAlineacion() {
+        System.out.println("Alineación:");
+        for (int i = 0; i < alineacion.length; i++) {
+            if (alineacion[i] != null) {
+                System.out.println(i + ": " + alineacion[i]);
+            } else {
+                System.out.println(i + ": Vacío");
+            }
+        }
+    }
+
+  public void verificarAlineacionCompleta() throws Exception {
+        for (Jugador jugador : alineacion) {
+            if (jugador == null) {
+                throw new Exception("No se ha completado la alineación.");
+            }
+        }
+    }
 }
