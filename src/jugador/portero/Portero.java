963c5cc2ef4597fa894c044ecb9e9e5a26bc9f4b
AdrianMorGar <159542087+AdrianMorGar@users.noreply.github.com>
2024-03-27 17:09:22 +0100

    v1ClasePortero.java
    Se crea las funciones de portero: sus get y set mas su método heredado de jugador

--- src/jugador/portero/Portero.java
+++ src/jugador/portero/Portero.java
@@ -1,5 +1,34 @@
 package jugador.portero;
 
-public class Portero {
+import jugador.Jugador;
 
+public class Portero extends Jugador {
+
+	private int paradas;
+
+	public Portero(String nombre, int dorsal, String equipo, int paradas) {
+		super(nombre, dorsal, equipo);
+		this.paradas = paradas;
+	}
+	
+	public int setParadas() {
+		return this.paradas;
+	}
+	
+	public void getPartidos(int paradas) {
+		this.paradas = paradas;
+	}
+	
+	@Override
+	public void mostrarDatos() {
+		System.out.print(this.toString());
+	}
+	
+	@Override
+	public String toString() {
+		return "Portero[nombre= " + this.getNombre() + 
+		", dorsal= " + this.getDorsal() + 
+		", equipo= " + this.getEquipo() +
+		", paradas= " + this.paradas + "]";
+	}
 }
