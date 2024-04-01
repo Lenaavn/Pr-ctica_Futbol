0f59c74cdacd3c509d6d09d4786aa4214ecbb9b8
AdrianMorGar <159542087+AdrianMorGar@users.noreply.github.com>
2024-04-01 22:21:10 +0200

    V3Equipo.java
    Agregación de los Getters y setters olvidados y que impedían que se pudiera realizar el main

--- src/equipo/Equipo.java
+++ src/equipo/Equipo.java
@@ -7,33 +7,41 @@ public class Equipo {
 	private Jugador[] alineacion;
 
 	public Equipo() {
-		this.alineacion = new Jugador[11];
+		this.setAlineacion(new Jugador[11]);
 	}
 
 	public void añadirJugador(Jugador jugador, int posicion) {
 
-		if (posicion < 0 || posicion >= alineacion.length) {
+		if (posicion < 0 || posicion >= getAlineacion().length) {
 			throw new IllegalArgumentException("La posición está fuera del rango de la alineación.");
 		} else {
-			for (Jugador jugadorAntiguo : alineacion) {
+			for (Jugador jugadorAntiguo : getAlineacion()) {
 				if (jugadorAntiguo != null && !jugadorAntiguo.getEquipo().equals(jugador.getEquipo())) {
 					throw new IllegalArgumentException("No se puede agregar el jugador " + jugador.getNombre()
 							+ " porque no pertenece al mismo equipo.");
 				}
 			}
-			alineacion[posicion] = jugador;
+			getAlineacion()[posicion] = jugador;
 		}
 	}
 
 	public void mostrarAlineacion() {
 		System.out.println("Alineación:");
-		for (int i = 0; i < alineacion.length; i++) {
-			if (alineacion[i] == null) {
+		for (int i = 0; i < getAlineacion().length; i++) {
+			if (getAlineacion()[i] == null) {
 				throw new IllegalArgumentException("No se ha completado la alineación.");
 			} else {
 				System.out.println(
-						i + ": Nombre: " + alineacion[i].getNombre() + ", Dorsal: " + alineacion[i].getDorsal());
+						i + ": Nombre: " + getAlineacion()[i].getNombre() + ", Dorsal: " + getAlineacion()[i].getDorsal());
 			}
 		}
+	}
+
+	public Jugador[] getAlineacion() {
+		return alineacion;
+	}
+
+	public void setAlineacion(Jugador[] alineacion) {
+		this.alineacion = alineacion;
 	}
 }
